03213565f0d1a33dd0b7bfed31d60f35e7f35f17
Robotics-2172 <robots@sehs.net>
2025-03-14 22:42:15 -0400

    hi

---
+++ app/src/main/java/com/example/java_yay/testagain.java
@@ -0,0 +1,19 @@
+package com.example.java_yay;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class testagain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.testagain);
+        findViewById(R.id.button2).setOnClickListener(v ->{
+            Intent intent = new Intent(this, mainActivity.class);
+            finish();
+      });
+    }
+}
