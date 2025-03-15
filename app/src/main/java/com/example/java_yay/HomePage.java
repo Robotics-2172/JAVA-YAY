e16a5bfcb778f9f2c93194ac53fb78236c03f6c0
Robotics-2172 <robots@sehs.net>
2025-03-15 04:08:00 -0400

    Grady

--- app/src/main/java/com/example/java_yay/HomePage.java
+++ app/src/main/java/com/example/java_yay/HomePage.java
@@ -13,10 +13,22 @@ import java.util.concurrent.CompletableFuture;
 
 public class HomePage extends Activity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.mainactivity);
-
+        findViewById(R.id.friendshipadvice).setOnClickListener(v->{
+            Intent intent = new Intent(this, FriendshipAdvicePage.class);
+            startActivity(intent);
+        });
+        findViewById(R.id.friendslist).setOnClickListener(v-> {
+            Intent intent = new Intent(this, FriendList.class);
+            startActivity(intent);
+        });
+        findViewById(R.id.hygiene).setOnClickListener(v->{
+            Intent intent = new Intent(this, HygienePage.class);
+            startActivity(intent);
+        });
     }
 }
