45b365e2cc7b655048fa21597931e3c0dd4a1a59
Robotics-2172 <robots@sehs.net>
2025-03-14 23:46:26 -0400

    Grady

--- app/src/main/java/com/example/java_yay/mainActivity.java
+++ app/src/main/java/com/example/java_yay/mainActivity.java
@@ -3,16 +3,23 @@ package com.example.java_yay;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.datastore.core.DataStore;
+
 public class mainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.mainactivity);
         findViewById(R.id.button).setOnClickListener(v->{
-            Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
+            EditText b = findViewById(R.id.editTextText);
+            Toast.makeText(this, b.getText(), Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this, test.class);
+
             startActivity(intent);
         });
     }
