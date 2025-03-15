5975b645e0d72250af858eeb68a1d00c28412b83
Robotics-2172 <robots@sehs.net>
2025-03-15 10:22:28 -0400

    Grady

--- app/src/main/java/com/example/java_yay/FriendList.java
+++ app/src/main/java/com/example/java_yay/FriendList.java
@@ -1,12 +1,17 @@
 package com.example.java_yay;
 
 import android.app.Activity;
+import android.content.ContextParams;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class FriendList extends Activity {
@@ -14,8 +19,36 @@ public class FriendList extends Activity {
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         FriendDB db = ((CraigApplication) getApplication()).getFriendDB();
-
-
+        setContentView(R.layout.friendslist);
+        LinearLayout panel = findViewById(R.id.linearlayout);
+        CompletableFuture.supplyAsync(() -> {
+            return db.friendDao().getAll();
+        }).thenAccept(l -> {
+                for (Friend f : l) {
+                    Log.i("Tag", String.valueOf(l.indexOf(f)));
+                    Button b = new Button(this);
+                    b.setText(f.name);
+                    b.setTranslationY(l.indexOf(f) * 20);
+                    CompletableFuture.runAsync(() -> {
+                                panel.addView(b);
+                            });
+                }
+            });
+        findViewById(R.id.addfriend).setOnClickListener(v -> {
+            Friend fr = new Friend();
+            EditText b = findViewById(R.id.name);
+            fr.name = b.getText().toString();
+            fr.birthday = "12/12/12";
+            CompletableFuture.runAsync(() -> {
+                db.friendDao().insertAll(fr);
+            }).exceptionally(throwable -> null).thenRun(() -> {
+                runOnUiThread(() -> {
+                    Toast.makeText(this, "Added!" + fr.name, Toast.LENGTH_SHORT).show();
+                });
+            });
+        });
+    }
+}
 //        findViewById(R.id.button).setOnClickListener(v -> {
 //
 //            CompletableFuture.supplyAsync(() -> {
@@ -59,5 +92,3 @@ public class FriendList extends Activity {
 //                ;
 //            });
 //        });
-    }
-}
