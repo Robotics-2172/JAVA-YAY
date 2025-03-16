5375cb529d09e1cd2636958d42bd82a10d22efd8
Robotics-2172 <robots@sehs.net>
2025-03-15 21:53:50 -0400

    grady

--- app/src/main/java/com/example/java_yay/FriendshipAdviceActivity.java
+++ app/src/main/java/com/example/java_yay/FriendshipAdviceActivity.java
@@ -4,13 +4,25 @@ import static com.example.java_yay.UtilityFunctions.startActivityWithButton;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.core.content.res.ResourcesCompat;
+
+import org.w3c.dom.Text;
 
 public class FriendshipAdviceActivity extends Activity {
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.friendship_advice_page);
+        TextView tx = findViewById(R.id.textView);
+
+        Typeface custom_font = ResourcesCompat.getFont(this, R.font.kay_pho_du);
+
+        tx.setTypeface(custom_font);
+
 
         startActivityWithButton(this, R.id.gettingToKnowButton, GettingToKnowActivity.class);
         startActivityWithButton(this, R.id.socialNormsButton, SocialNormsActivity.class);
